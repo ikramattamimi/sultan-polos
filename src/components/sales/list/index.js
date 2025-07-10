0f580e3e3040672ae2340252ba43fe380072d317
ikramattamimi <attamimiikram@gmail.com>
2025-07-11 00:38:38 +0700

    fix(project structure) - sales list index

--- src/components/sales/list/index.js
+++ src/components/sales/list/index.js
@@ -1,4 +1,4 @@
 export { default as SalesFilter } from "./SalesFilter.jsx";
 export { default as SalesPagination } from "./SalesPagination.jsx";
 export { default as SalesTable } from "./SalesTable.jsx";
-export { default as SalesTableRow } from "./salesTableRow.jsx";
+export { default as SalesTableRow } from "./SalesTableRow.jsx";
